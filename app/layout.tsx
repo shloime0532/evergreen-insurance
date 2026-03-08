71620241a6113e230463ff91ae6e30f7b8f7cd0a
Shloime Rokeach <info@anchorpointnj.com>
2026-03-07 22:34:23 -0500

    feat: add footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Nav from "./components/nav";
+import Footer from "./components/footer";
 import "./globals.css";
 
 const inter = Inter({
@@ -31,7 +32,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
         <Nav />
-        {children}
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
